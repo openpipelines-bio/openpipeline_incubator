f7b1fb117c0890fc0691eff50eeb0cab610b54dc
jakubmajercik <jakub.majercik@gmail.com>
2025-02-20 14:46:43 +0100

    WIP reporting component added

---
+++ src/ingestion_qc/generate_html/script.sh
@@ -0,0 +1,14 @@
+cd /opt/incubator_ingestion_qc
+mkdir -p data
+mkdir -p dist
+mkdir -p src/data
+cp $par_input /opt/incubator_ingestion_qc/data/dataset.json
+
+echo "Compressing input data..."
+pnpm run compress_data
+
+echo "Generating HTML..."
+pnpm run build
+
+echo "Copying HTML to output..."
+cp dist/index.html $par_output
