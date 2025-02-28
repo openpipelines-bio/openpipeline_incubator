056f5e7769bb2973474d5b9dbdae96b8c0b0b4e2
jakubmajercik <jakub.majercik@gmail.com>
2025-02-28 10:58:37 +0100

    updated testing thresholds

--- src/ingestion_qc/h5mu_to_qc_json/test.py
+++ src/ingestion_qc/h5mu_to_qc_json/test.py
@@ -46,8 +46,9 @@ def test_set_filters(run_component, tmp_path):
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_one.qc.h5mu",
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_two.qc.h5mu",
             "--output", output_json_path,
-            "--min_total_counts", "100",
-            "--min_num_nonzero_vars", "100",
+            "--sample_id_key", "sample_id",
+            "--min_total_counts", "10",
+            "--min_num_nonzero_vars", "10",
             "--obs_keys", "total_counts", 
             "--obs_keys", "num_nonzero_vars"
         ]
@@ -69,10 +70,10 @@ def test_set_filters(run_component, tmp_path):
             assert {"name", "dtype", "data"}.issubset(col.keys())
             
     total_counts = next(col for col in output_json_dict["cell_rna_stats"]["columns"] if col["name"] == "total_counts")
-    assert min(total_counts["data"]) >= 100
+    assert min(total_counts["data"]) >= 10
     
     num_nonzero_vars = next(col for col in output_json_dict["cell_rna_stats"]["columns"] if col["name"] == "num_nonzero_vars")
-    assert min(num_nonzero_vars["data"]) >= 100
+    assert min(num_nonzero_vars["data"]) >= 10
     
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__]))
