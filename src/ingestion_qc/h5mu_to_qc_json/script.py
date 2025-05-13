c374fb74703b353ca8b4b56791deaf97bae14987
dorien-er <roosen.dorien@gmail.com>
2025-05-13 17:03:21 +0200

    wip

--- src/ingestion_qc/h5mu_to_qc_json/script.py
+++ src/ingestion_qc/h5mu_to_qc_json/script.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import anndata as ad
 import h5py
 import sys
+import os
+import shutil
 
 ## VIASH START
 # inputs = list(Path("data/sample_data/sample_data").glob("*.h5mu"))
@@ -13,8 +15,15 @@ output = "tmp.json"
 par = {
     "input": sorted([str(x) for x in inputs]),
     "output": output,
+    "output_reporting_json": "cr_struct.json",
     "modality": "rna",
-    "sample_id_key": "sample_id",
+    "input_obs_sample_id_key": "sample_id",
+    "ingestion_method": "cellranger_multi",
+    "input_obs_sample_id_key": "sample_id",
+    "input_obs_total_counts_key": "total_counts",
+    "input_obs_num_nonzero_vars_key": "num_nonzero_vars",
+    "input_obs_fraction_mitochondrial_key": "fraction_mitochondrial",
+    "input_obs_fraction_ribosomal_key": "fraction_ribosomal",
     "min_total_counts": 10,
     "min_num_nonzero_vars": 10,
     "obs_keys": [
@@ -28,12 +37,17 @@ par = {
         "cellbender_cell_probability",
         "cellbender_cell_size",
         "cellbender_droplet_efficiency",
-    ],        
+    ],
     "cellranger_metrics_uns_key": "metrics_cellranger",
     "metadata_obs_keys": []
 }
+meta = {
+    "resources_dir": os.path.abspath("src/ingestion_qc/h5mu_to_qc_json/report_structure"),
+}
 i = 0
 mudata_file = par["input"][i]
+
+sys.path.append("src/utils")
 ## VIASH END
 
 sys.path.append(meta["resources_dir"])
@@ -43,13 +57,13 @@ logger = setup_logger()
 
 par["cellbender_obs_keys"] = {} if not par["cellbender_obs_keys"] else par["cellbender_obs_keys"]
 par["metadata_obs_keys"] = {} if not par["metadata_obs_keys"] else par["metadata_obs_keys"]
-par["obs_keys"] = {} if not par["obs_keys"] else par["obs_keys"]
+# par["obs_keys"] = {} if not par["obs_keys"] else par["obs_keys"]
+
 
 def transform_df(df):
     """Transform a DataFrame into the annotation object format."""
     columns = []
     for name in df.columns:
-        print(f"Processing column {name}")
         data = df[name]
 
         # Determine dtype
@@ -74,20 +88,80 @@ def transform_df(df):
 
     return {"num_rows": len(df), "num_cols": len(df.columns), "columns": columns}
 
+
+def detect_categorical_keys(df):
+    pass
+
+
 def check_optional_obs_keys(obs, keys, message):
     missing_keys = [key for key in keys if key not in obs.columns]
     if missing_keys:
         logger.info(f"Missing keys in obs: {', '.join(missing_keys)}. {message}")
 
-def main(par):    
+
+def transform_cellranger_metrics(uns, sample_id):
+    if not par["cellranger_metrics_uns_key"] in uns:
+        raise ValueError(f"Could not find cellranger metrics in uns: {par['cellranger_metrics_uns_key']}. Provide correct value for --cellranger_metrics_uns_key or make sure data was ingested using CellRanger multi.")
+
+    cellranger_metrics = (
+        uns[par["cellranger_metrics_uns_key"]]
+        .pivot_table(
+            index=[],
+            columns="Metric Name",
+            values="Metric Value",
+            aggfunc="first",
+        )
+        .reset_index(drop=True)
+    )
+
+    cellranger_metrics.columns.name = None
+    # Remove thousands separator and convert to numeric
+    cellranger_metrics = cellranger_metrics.map(
+        lambda x: (
+            pd.to_numeric(x.replace(",", ""), errors="coerce")
+            if isinstance(x, str)
+            else x
+        )
+    )
+    # Replace spaces with underscores in column names
+    cellranger_metrics.columns = cellranger_metrics.columns.str.replace(" ", "_")
+    for col in cellranger_metrics.columns:
+        cellranger_metrics[col] = pd.to_numeric(cellranger_metrics[col], errors="coerce")
+    cellranger_metrics["sample_id"] = [sample_id[0]]
+
+    return cellranger_metrics
+
+
+def generate_cellranger_stats(mod_obs, uns, sample_id, required_keys):
+
+    # Check if celbender was run on the dataset
+    if par["cellbender_obs_keys"]:
+        check_optional_obs_keys(mod_obs, par["cellbender_obs_keys"], "Run cellbender first to include these metrics.")
+    if par["metadata_obs_keys"]:
+        check_optional_obs_keys(mod_obs, par["metadata_obs_keys"], "Make sure requested metadata colmuns are present in obs.")
+
+    # Create cell RNA stats dataframe
+    cell_rna_stats = pd.DataFrame(
+        {
+            "sample_id": pd.Categorical(sample_id),
+            **{key: mod_obs[key] for key in required_keys},
+            **{key: mod_obs[key] for key in par["cellbender_obs_keys"] if key in mod_obs.columns},
+            **{key: mod_obs[key] for key in par["metadata_obs_keys"] if key in mod_obs.columns},
+        }
+    )
+
+    cellranger_stats = transform_cellranger_metrics(uns, sample_id)
+
+    return cell_rna_stats, cellranger_stats
+
+
+def main(par):
     cell_stats_dfs = []
     sample_stats_dfs = []
     metrics_cellranger_dfs = []
 
-    print(par["input"])
-
     for i, mudata_file in enumerate(par["input"]):
-        print(f"Processing {mudata_file}")
+        logger.info(f"Processing {mudata_file}")
 
         # read h5mu file
         file = h5py.File(mudata_file, "r")
@@ -109,89 +183,74 @@ def main(par):
             mod_obs = mod_obs[mod_obs["num_nonzero_vars"] >= par["min_num_nonzero_vars"]]
         barcodes_filtered_count = mod_obs.shape[0]
 
-        missing_keys = [key for key in par["obs_keys"] if key not in mod_obs.columns]
-        if missing_keys:
-            raise ValueError(f"Missing keys in obs: {', '.join(missing_keys)}")
-        
-        if par["cellbender_obs_keys"]:
-            check_optional_obs_keys(mod_obs, par["cellbender_obs_keys"], "Run cellbender first to include these metrics.")
-        if par["metadata_obs_keys"]:
-            check_optional_obs_keys(mod_obs, par["metadata_obs_keys"], "Make sure requested metadata colmuns are present in obs.")
-
         sample_id = (
-            mod_obs[par["sample_id_key"]].tolist()
-            if par["sample_id_key"] in mod_obs.columns
+            mod_obs[par["input_obs_sample_id_key"]].tolist()
+            if par["input_obs_sample_id_key"] in mod_obs.columns
             else [f"sample_{i}"] * mod_obs.shape[0]
         )
 
-        cell_rna_stats = pd.DataFrame(
-            {
-                "sample_id": pd.Categorical(sample_id),
-                **{key: mod_obs[key] for key in par["obs_keys"]},
-                **{key: mod_obs[key] for key in par["cellbender_obs_keys"] if key in mod_obs.columns},
-                **{key: mod_obs[key] for key in par["metadata_obs_keys"] if key in mod_obs.columns},
-            }
-        )
-        
+        required_keys = [
+            par["input_obs_total_counts_key"],
+            par["input_obs_num_nonzero_vars_key"],
+            par["input_obs_fraction_mitochondrial_key"],
+            par["input_obs_fraction_ribosomal_key"]
+            ]
+        missing_keys = [key for key in required_keys if key not in mod_obs.columns]
+        if missing_keys:
+            raise ValueError(f"Missing keys in obs: {', '.join(missing_keys)}")
+
         sample_summary_stats = pd.DataFrame(
             {
                 "sample_id": pd.Categorical([sample_id[0]]),
                 "rna_num_barcodes": [barcodes_original_count],
                 "rna_num_barcodes_filtered": [barcodes_filtered_count],
-                "rna_sum_total_counts": [mod_obs["total_counts"].sum()],
-                "rna_median_total_counts": [mod_obs["total_counts"].median()],
-                "rna_overall_num_nonzero_vars": [mod_obs["num_nonzero_vars"].sum()],
-                "rna_median_num_nonzero_vars": [mod_obs["num_nonzero_vars"].median()],
+                "rna_sum_total_counts": [mod_obs[par["input_obs_total_counts_key"]].sum()],
+                "rna_median_total_counts": [mod_obs[par["input_obs_total_counts_key"]].median()],
+                "rna_overall_num_nonzero_vars": [mod_obs[par["input_obs_num_nonzero_vars_key"]].sum()],
+                "rna_median_num_nonzero_vars": [mod_obs[par["input_obs_num_nonzero_vars_key"]].median()],
             }
         )
 
-        if par["cellranger_metrics_uns_key"] in uns:
-            metrics = (
-                uns[par["cellranger_metrics_uns_key"]]
-                .pivot_table(
-                    index=[],
-                    columns="Metric Name",
-                    values="Metric Value",
-                    aggfunc="first",
-                )
-                .reset_index(drop=True)
-            )
+        if par["ingestion_method"] == "cellranger_multi":
+            cell_rna_stats, cellranger_stats = generate_cellranger_stats(mod_obs, uns, sample_id, required_keys)
+            metrics_cellranger_dfs.append(cellranger_stats)
 
-            metrics.columns.name = None
-            # Remove thousands separator and convert to numeric
-            metrics = metrics.map(
-                lambda x: (
-                    pd.to_numeric(x.replace(",", ""), errors="coerce")
-                    if isinstance(x, str)
-                    else x
-                )
-            )
-            # Replace spaces with underscores in column names
-            metrics.columns = metrics.columns.str.replace(" ", "_")
-            for col in metrics.columns:
-                metrics[col] = pd.to_numeric(metrics[col], errors="coerce")
-            metrics["sample_id"] = [sample_id[0]]
-            metrics_cellranger_dfs.append(metrics)
-        
         cell_stats_dfs.append(cell_rna_stats)
         sample_stats_dfs.append(sample_summary_stats)
 
+    # Combine dataframes of all samples
     combined_cell_stats = pd.concat(cell_stats_dfs, ignore_index=True)
     combined_sample_stats = pd.concat(sample_stats_dfs, ignore_index=True)
-    combined_metrics_cellranger = pd.concat(metrics_cellranger_dfs, ignore_index=True)
+    if par["ingestion_method"] == "cellranger_multi":
+        combined_metrics_cellranger = pd.concat(metrics_cellranger_dfs, ignore_index=True)
 
-    for df in [combined_cell_stats, combined_sample_stats, combined_metrics_cellranger]:
+    report_categories = [combined_cell_stats, combined_sample_stats]
+
+    if par["ingestion_method"] == "cellranger_multi":
+        report_categories.append(combined_metrics_cellranger)
+
+    for df in report_categories:
         df["sample_id"] = pd.Categorical(df["sample_id"])
 
     output = {
         "cell_rna_stats": transform_df(combined_cell_stats),
-        "sample_summary_stats": transform_df(combined_sample_stats),
-        "metrics_cellranger_stats": transform_df(combined_metrics_cellranger),
+        "sample_summary_stats": transform_df(combined_sample_stats)
     }
 
+    if par["ingestion_method"] == "cellranger_multi":
+        output["metrics_cellranger_stats"] = transform_df(combined_metrics_cellranger)
+
+    logger.info(f"Writing output to {par['output']}")
     output_path = Path(par["output"])
     with open(output_path, "w") as f:
         json.dump(output, f, indent=2)
+
+    report_structures = {
+        "cellranger_multi": os.path.join(meta["resources_dir"], "report_structure/cellranger.json"),
+        "xenium": os.path.join(meta["resources_dir"], "report_structure/xenium.json")
+    }
+
+    shutil.copy(report_structures[par["ingestion_method"]], par["output_reporting_json"])
 
 
 if __name__ == "__main__":
--- src/ingestion_qc/h5mu_to_qc_json/test.py
+++ src/ingestion_qc/h5mu_to_qc_json/test.py
@@ -1,8 +1,8 @@
 import pytest
-import mudata as mu
 import os
 import json
 import sys
+import numpy as np
 
 ## VIASH START
 meta = {
@@ -11,72 +11,90 @@ meta = {
 }
 ## VIASH END
 
+
 def test_simple_execution(run_component, tmp_path):
     output_json_path = tmp_path / "output.json"
-    
+    output_reporting_json_path = tmp_path / "output_reporting.json"
+
     run_component(
         [
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_one.qc.cellbender.h5mu",
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_two.qc.cellbender.h5mu",
+            "--ingestion_method", "cellranger_multi",
             "--output", output_json_path,
+            "--output_reporting_json", output_reporting_json_path
         ]
     )
-    
+
     assert os.path.exists(output_json_path), "Output file was not created"
-    
+
     with open(output_json_path, "r") as f:
         output_json_dict = json.load(f)
-    
+
     assert output_json_dict.keys() == {"cell_rna_stats", "sample_summary_stats", "metrics_cellranger_stats"}
-    
+
     column_names_cell = [col["name"] for col in output_json_dict["cell_rna_stats"]["columns"]]
-    assert column_names_cell == ["sample_id", "total_counts", "num_nonzero_vars", "fraction_mitochondrial", "fraction_ribosomal",
-                                 "cellbender_background_fraction", "cellbender_cell_probability", "cellbender_cell_size",
-                                 "cellbender_droplet_efficiency"]
-        
+    expected_column_names = [
+        "sample_id", "total_counts", "num_nonzero_vars",
+        "fraction_mitochondrial",  "fraction_ribosomal",
+        "cellbender_background_fraction", "cellbender_cell_probability",
+        "cellbender_cell_size", "cellbender_droplet_efficiency"
+        ]
+    assert np.all([column in column_names_cell for column in expected_column_names])
+
     for key in output_json_dict.keys():
         assert output_json_dict[key].keys() == {"num_rows", "num_cols", "columns"}
         for col in output_json_dict[key]["columns"]:
             assert {"name", "dtype", "data"}.issubset(col.keys())
-        
+
 
 def test_set_filters(run_component, tmp_path):
     output_json_path = tmp_path / "output.json"
-    
+    output_reporting_json_path = tmp_path / "output_reporting.json"
+
     run_component(
         [
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_one.qc.cellbender.h5mu",
             "--input", meta["resources_dir"] + "/resources_test/qc_sample_data/sample_two.qc.cellbender.h5mu",
+            "--ingestion_method", "cellranger_multi",
             "--output", output_json_path,
-            "--sample_id_key", "sample_id",
-            "--min_total_counts", "10",
-            "--min_num_nonzero_vars", "10",
-            "--obs_keys", "total_counts", 
-            "--obs_keys", "num_nonzero_vars"
+            "--output_reporting_json", output_reporting_json_path,
+            "--input_obs_sample_id_key", "sample_id",
+            "--input_obs_total_counts_key", "total_counts",
+            "--input_obs_num_nonzero_vars_key", "num_nonzero_vars",
+            "--input_obs_fraction_mitochondrial_key", "fraction_mitochondrial",
+            "--input_obs_fraction_ribosomal_key", "fraction_ribosomal",
+            "--min_total_counts", "20",
+            "--min_num_nonzero_vars", "20"
         ]
     )
-        
+
     assert os.path.exists(output_json_path), "Output file was not created"
-    
+
     with open(output_json_path, "r") as f:
         output_json_dict = json.load(f)
-    
+
     assert output_json_dict.keys() == {"cell_rna_stats", "sample_summary_stats", "metrics_cellranger_stats"}
-    
+
     column_names = [col["name"] for col in output_json_dict["cell_rna_stats"]["columns"]]
-    assert column_names == ["sample_id", "total_counts", "num_nonzero_vars", "cellbender_background_fraction",
-                            "cellbender_cell_probability", "cellbender_cell_size", "cellbender_droplet_efficiency"]
-        
+    expected_column_names = [
+        "sample_id", "total_counts", "num_nonzero_vars",
+        "fraction_mitochondrial",  "fraction_ribosomal",
+        "cellbender_background_fraction", "cellbender_cell_probability",
+        "cellbender_cell_size", "cellbender_droplet_efficiency"
+        ]
+    assert np.all([column in column_names for column in expected_column_names])
     for key in output_json_dict.keys():
         assert output_json_dict[key].keys() == {"num_rows", "num_cols", "columns"}
         for col in output_json_dict[key]["columns"]:
             assert {"name", "dtype", "data"}.issubset(col.keys())
-            
+
     total_counts = next(col for col in output_json_dict["cell_rna_stats"]["columns"] if col["name"] == "total_counts")
-    assert min(total_counts["data"]) >= 10
-    
+    assert min(total_counts["data"]) >= 20
+
     num_nonzero_vars = next(col for col in output_json_dict["cell_rna_stats"]["columns"] if col["name"] == "num_nonzero_vars")
-    assert min(num_nonzero_vars["data"]) >= 10
-    
+    assert min(num_nonzero_vars["data"]) >= 20
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__]))
